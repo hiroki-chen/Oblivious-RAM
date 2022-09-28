f5ab90b3fd6e67ec94b62d2e92337d9157b9ec99
Haobin Chen <etyyuiope@qq.com>
2022-09-28 06:26:37 -0400

    Refine v1.2.0

--- base/oram_utils.h
+++ base/oram_utils.h
@@ -40,6 +40,11 @@ To* TryCast(From* const from) {
   return to;
 }
 
+template <typename E>
+constexpr typename std::underlying_type<E>::type to_underlying(E e) noexcept {
+  return static_cast<typename std::underlying_type<E>::type>(e);
+}
+
 template <typename... Args>
 std::string StrCat(Args&&... args) {
   std::ostringstream oss;
